d2dcc3b285137d341f82275a446ea1d04fd47c39
martha danladi <marthadanladi>
2022-12-02 18:18:35 +0100

    beginning javascript

---
+++ JavaScriptLearning/index.js
@@ -0,0 +1,41 @@
+console.log("WORKING");
+
+let firstName = "Bolaji";
+var age = 12;
+const API_key = "n123jfnnssn";
+
+firstName = "Dean";
+lastName = "Michael"
+console.log("firstName");
+alert(firstName + " " + lastName)
+
+console.log(typeof);
+
+console.log(`${firstName} ${lastName}`)
+let x = {}
+let y = new objst
+
+let user = {
+    id: "SKU-1234",
+    surname: "Kayode",
+    firstName: "Mayowa",
+    phoneNumber: "08099929384"
+}
+
+console.log(user.id);
+console.log(user["id"]);
+
+console.log(Object.keys(user))
+console.log(Object.values(user))
+
+let userId  = prompt("WHat's your id number? ")
+user.id = userId
+let firstName  = prompt("WHat's your id number? ")
+user.firstName = firstName
+let surname  = prompt("WHat's your id number? ")
+surname = surname
+let phoneNumber  = prompt("WHat's your id number? ")
+phoneNumber = phoneNumber
+
+
+
